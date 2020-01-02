e0865db3d962941fbb2ed9c304012b02a91184b4
Brian Haferkamp <brian.haferkamp@gmail.com>
2020-01-02 11:53:44 -0600

    Added video modal

--- assets/js/app.js
+++ assets/js/app.js
@@ -91,8 +91,23 @@ $(document).ready(function() {
       readingPlanURL = 'https://www.paperturn-view.com/us/tyndale-bibles/december-pray-for-america-reading-plan?pid=NjE61814&v=1.11';
 
     } else if (month === 1) {
-      month = 'December';
-      readingPlanURL = 'https://www.paperturn-view.com/us/tyndale-bibles/sampler-one-year-pray-for-america-bible?pid=NTc57082&v=2.15';
+      month = 'January';
+      readingPlanURL = 'https://www.paperturn-view.com/us/tyndale-bibles/sampler-one-year-pray-for-america-bible?pid=NTc57082&v=2.16';
+    }
+
+    else if (month === 2) {
+      month = 'February';
+      readingPlanURL = 'https://www.paperturn-view.com/us/tyndale-bibles/february-one-year-pray-for-america-bible?pid=Njg68595&v=1.13';
+    }
+
+    else if (month === 3) {
+      month = 'March';
+      readingPlanURL = 'https://www.paperturn-view.com/us/tyndale-bibles/march-one-year-pray-for-america-reading-plan?pid=NzE71784&v=1.10';
+    }
+
+    else if (month === 4) {
+      month = 'March';
+      readingPlanURL = 'https://www.paperturn-view.com/?pid=NzE71791&v=1.10';
     }
 
     $('.thanks-button').attr('href', readingPlanURL);
@@ -110,6 +125,32 @@ $(document).ready(function() {
     $('.button-dropdown-list-item').on('click', function() {
       $('.retailer-button').removeClass('active');
       $('.button-dropdown-list').hide();
+    });
+
+
+    //---------------------------------------------------
+    // Popup Ad
+    //---------------------------------------------------
+
+    $('#popup-ad').css('display', 'flex');
+
+    $('.popup-ad-close, .popup-ad-image').on('click', function() {
+      $('#popup-ad').css('display', '');
+    });
+
+
+    //---------------------------------------------------
+    // Rebecca St. James Video modal
+    //---------------------------------------------------
+
+    $('.video-trigger').on('click', function() {
+      $('.overlay').fadeIn(300);
+      $('#video-modal').addClass('active');
+    });
+
+    $('.video-modal-close').on('click', function() {
+      $('.overlay').fadeOut(300);
+      $('#video-modal').removeClass('active');
     });
 
 
